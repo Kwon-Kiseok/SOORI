ec7838c50fb32a2b7ea9685e80ca98b473438990
Kwon-Kiseok <40809467+Kwon-Kiseok@users.noreply.github.com>
2018-07-12 11:34:42 +0900

    2018.07.12 git test

---
+++ Assets/Script/Character/Move/DashAbility.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashAbility : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    public float dashSpeed;
+    private float dashTime;
+    public float startDashTime;
+    public int direction;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        dashTime = startDashTime;
+    }
+    void Update()
+    {
+        //-------[dash Function]
+        //1일 경우 왼쪽 2일 경우 오른쪽
+        //방향키를 누른 상태 + 왼쪽 쉬프트 한번 클릭으로 대쉬 
+        if (direction == 0)
+        {
+            if((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                direction = 1;
+                animator.SetTrigger("doDash");
+            }
+            else if((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) && Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                direction = 2;
+                animator.SetTrigger("doDash");
+            }
+        }
+        else
+        {
+            if(dashTime <= 0)
+            {
+                direction = 0;
+                dashTime = startDashTime;
+                rb.velocity = Vector2.zero;
+            }
+            else
+            {
+                dashTime -= Time.deltaTime;
+                
+                if(direction == 1)
+                {
+                    rb.velocity = Vector2.left * dashSpeed;
+                }
+                else if(direction == 2)
+                {
+                    rb.velocity = Vector2.right * dashSpeed;
+                }
+            }
+            
+        }
+    }
+}
---
+++ Assets/Script/Character/Move/PlayerController.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float movePower = 1f;
+    public float jumpPower = 1f;
+    public int jumpCount = 2;
+
+    Rigidbody2D rigid;
+    Animator animator;
+
+    Vector3 movement;
+    bool isJumping = false;
+
+	void Start () {
+        rigid = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
+        jumpCount = 0; //시작부터 점프하는 것 방지
+	}
+
+	void Update () {
+        
+        //가만히 서있을 경우
+        if(Input.GetAxisRaw("Horizontal") == 0)
+        {
+            animator.SetBool("isMoving", false);
+        }
+        //왼쪽 이동
+        else if(Input.GetAxisRaw("Horizontal") < 0)
+        {
+            animator.SetBool("isMoving", true);
+        }
+        //오른쪽 이동
+        else if(Input.GetAxisRaw("Horizontal") > 0)
+        {
+            animator.SetBool("isMoving",true); 
+        }
+        //점프
+        if (jumpCount > 0)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                jumpCount--;
+                isJumping = true;
+                animator.SetBool("isJumping", true);
+                animator.SetTrigger("doJumping");
+            }
+        }
+	}
+
+    void FixedUpdate()
+    {
+        Move();
+        Jump();
+    }
+
+
+
+    //-------[Moving Function]---------------
+    void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            moveVelocity = Vector3.left;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            moveVelocity = Vector3.right;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        transform.position += moveVelocity * movePower * Time.deltaTime;
+    }
+    //-------[Jumping Function]---------------
+    void Jump()
+    {
+        if (!isJumping)
+            return;
+
+        rigid.velocity = Vector2.zero;
+
+        Vector2 jumpVelocity = new Vector2(0, jumpPower);
+        rigid.AddForce(jumpVelocity, ForceMode2D.Impulse);
+
+        isJumping = false;
+    }
+
+
+    //-------[Landing Function]---------------
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Attach : " + other.gameObject.layer);
+        if(other.gameObject.layer == 8 && rigid.velocity.y < 0)
+        {
+            animator.SetBool("isJumping", false);
+            jumpCount = 2;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Detach : " + other.gameObject.layer);
+    }
+}
