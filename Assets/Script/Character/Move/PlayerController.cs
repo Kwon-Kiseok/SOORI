3a28f4f2bafbfed06c1c09f418075d99233a6616
Kwon-Kiseok <40809467+Kwon-Kiseok@users.noreply.github.com>
2018-07-12 17:04:54 +0900

    2018.07.12 대쉬 쿨타임 구현

--- Assets/Script/Character/Move/PlayerController.cs
+++ Assets/Script/Character/Move/PlayerController.cs
@@ -4,20 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+
+    //--Move elements
     public float movePower = 1f;
+
+    //--Jump elements
     public float jumpPower = 1f;
     public int jumpCount = 2;
-
-    Rigidbody2D rigid;
-    Animator animator;
-
-    Vector3 movement;
     bool isJumping = false;
 
-	void Start () {
-        rigid = gameObject.GetComponent<Rigidbody2D>();
-        animator = gameObject.GetComponent<Animator>();
+    //--Dash elements
+    public float dashSpeed;
+    public float startDashTime;
+    public int direction;
+    private float dashCoolTime;
+    private float dashTime;
+
+    //Components
+    Rigidbody2D rigid;
+    Animator animator;
+    Vector3 movement;
+
+    void Awake()
+    {
+        dashCoolTime = 0.01f; //대쉬 쿨타임
         jumpCount = 0; //시작부터 점프하는 것 방지
+    }
+
+    void Start () {
+        rigid = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();     
+
+        dashTime = startDashTime;
+        
 	}
 
 	void Update () {
@@ -48,19 +67,27 @@ public class PlayerController : MonoBehaviour {
                 animator.SetTrigger("doJumping");
             }
         }
+
+        
 	}
 
     void FixedUpdate()
     {
         Move();
+        Dash();
         Jump();
     }
 
-
+    void LateUpdate()
+    {
+        if (dashCoolTime > 0)
+            dashCoolTime -= Time.smoothDeltaTime;
+    }
 
     //-------[Moving Function]---------------
     void Move()
     {
+
         Vector3 moveVelocity = Vector3.zero;
 
         if (Input.GetAxisRaw("Horizontal") < 0)
@@ -105,4 +132,50 @@ public class PlayerController : MonoBehaviour {
     {
         Debug.Log("Detach : " + other.gameObject.layer);
     }
+
+    //-------[Dash Function]---------------
+    void Dash()
+    {
+        if (dashCoolTime > 0)
+            return;
+
+            if (direction == 0)
+            {
+                if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && Input.GetKeyDown(KeyCode.LeftShift))
+                {
+                    direction = 1;
+                    animator.SetBool("isDash", true);
+                }
+                else if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) && Input.GetKeyDown(KeyCode.LeftShift))
+                {
+                    direction = 2;
+                    animator.SetBool("isDash", true);             
+            }
+            } else
+            {
+                if (dashTime <= 0)
+                {
+                    direction = 0;
+                    dashTime = startDashTime;
+                    rigid.velocity = Vector2.zero;
+                    dashCoolTime = 2f;            //대쉬 쿨타임 
+                    animator.SetBool("isDash", false);
+                }
+                else
+                {
+                    dashTime -= Time.deltaTime;
+
+                    if (direction == 1)
+                    {
+                        rigid.velocity = Vector2.left * dashSpeed;
+                    }
+                    else if (direction == 2)
+                    {
+                        rigid.velocity = Vector2.right * dashSpeed;
+                    }
+                }
+            }
+        
+    }
+
 }
